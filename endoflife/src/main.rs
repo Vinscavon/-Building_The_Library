bbda1d4fc5a087b833c66fe3a2b90adbf6bc91a1
Vincent Kiprop <85450187+Vinscavon@users.noreply.github.com>
2024-06-17 07:33:09 +0000

    yes

---
+++ endoflife/src/main.rs
@@ -0,0 +1,26 @@
+use endoflife::rust;
+
+use serde_json;
+
+fn main() {
+    let json_str: &str = r#"{
+    "releaseDate":"2024-05-02",
+    "eol":false,
+    "latest":"1.78.0",
+    "latestReleaseDate":"2024-05-02",
+    "lts":false
+    }"#;
+
+    let json_object = serde_json::from_str::<RustSingleCircle>(json_str);
+   
+    println!(
+        "{:?}",
+        json_object
+    );
+
+    if let Ok(data: RustSingleCircle) = json_object {
+        println!("{:#?}, data");
+    } else {
+        println!("Not able to parse to json. Invalid data format?");
+    }
+}
---
+++ endoflife/src/rust.rs
@@ -0,0 +1,15 @@
+use serde::{
+    Serialize, Deserialize
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+
+pub struct RustSingleCircle {
+ pub release_date: Option<String>,
+ pub eol: bool,
+ pub lastest: Option<String>,
+ pub lastest_release: Option<String>,
+ pub lts: bool
+}
